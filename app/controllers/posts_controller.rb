d407e88e147d1cd0947e591a05d70f0036dae709
8Bts <mcmizze@yahoo.com>
2020-07-24 17:19:30 +0400

    Add actions for PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end  
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+
+      redirect_to @post
+    else
+      render :new
+    end  
+  end  
 end
